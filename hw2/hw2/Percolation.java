231a6f2a389b9a5a50117600ac7d196df493db27
Xiaotong Diao <xidi9223@colorado.edu>
2020-06-06 22:45:53 -0600

    aded hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,81 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+enum state {
+    open, block;
+}
 public class Percolation {
+
+    private int size;
+    private state[][] sites;
+    private WeightedQuickUnionUF disjointSets;
+    private int openNum;
+
+    public Percolation(int N){
+        this.size = N;
+
+        this.sites = new state[N][N];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                this.sites[i][j] = state.block;
+            }
+        }
+        this.disjointSets = new WeightedQuickUnionUF(N * N + 2);
+        this.openNum = 0;
+
+    }
+
+    public void open(int row, int col){
+        if(this.sites[row][col] == state.open)
+            return;
+        this.openNum++;
+        this.sites[row][col] = state.open;
+
+        if(row > 0 && this.sites[row - 1][col] == state.open)
+            this.disjointSets.union(row * this.size + col, (row - 1) * this.size + col);
+        if(row < this.size - 1 && this.sites[row + 1][col] == state.open)
+            this.disjointSets.union(row * this.size + col, (row + 1) * this.size + col);
+        if(col > 0 && this.sites[row][col - 1] == state.open)
+            this.disjointSets.union(row * this.size + col, row * this.size + col - 1);
+        if(col < this.size - 1 && this.sites[row][col + 1] == state.open)
+            this.disjointSets.union(row * this.size + col, row * this.size + col + 1);
+
+        if(row == 0)
+            this.disjointSets.union(this.size * this.size, row * this.size + col);
+
+        if(row == this.size - 1)
+            this.disjointSets.union(this.size * this.size + 1, row * this.size + col);
+    }
+
+    public boolean isOpen(int row, int col){
+        return this.sites[row][col] == state.open;
+    }
+
+    public boolean isFull(int row, int col){
+        return this.disjointSets.connected(row * this.size + col, this.size * this.size);
+    }
+
+    public int numberOfOpenSites(){
+        return this.openNum;
+    }
+
+    public boolean percolates(){
+        return this.disjointSets.connected(this.size * this.size, this.size * this.size + 1);
+    }
+
+    public static void main(String[] args){
+        int test1 = 10;
+        Percolation p = new Percolation(test1);
+        for(int i = 0; i < test1; i++){
+            p.open(i, i);
+        }
+        for(int i = 0; i < test1; i++){
+            p.open(i, test1 - i - 1);
+        }
+        for(int i = 0; i < test1; i++){
+            p.open(i, 4);
+        }
+        PercolationVisualizer.draw(p, test1);
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,59 @@
 package hw2;
 
-public class PercolationStats {
+import java.util.Random;
 
+public class PercolationStats {
+    private double mean;
+    private double std;
+    private double confLow;
+    private double confHigh;
+
+    public PercolationStats(int N, int T, PercolationFactory pf){ // perform T independent experiments on an N-by-N grid
+        this.mean = 0;
+        this.std = 0;
+        double[] sample = new double[T];
+        for(int i = 0; i < T; i++){
+            Percolation p = pf.make(N);
+            int count = 0;
+            while(!p.percolates()){
+                //randomly open a site
+                Random rand = new Random();
+                int rand1 = rand.nextInt(N*N);
+                if(!p.isOpen(rand1 / N, rand1 % N)) {
+                    p.open(rand1 / N, rand1 % N);
+                    count++;
+                }
+            }
+            double f = (double)count / (N * N);
+            this.mean += f;
+            sample[i] = f;
+        }
+        this.mean /= T;
+        for(int i = 0; i < T; i++){
+            this.std += Math.pow(sample[i] - this.mean, 2);
+        }
+        this.std = Math.sqrt(this.std / (T-1));
+        this.confLow = this.mean - (1.96 * this.std / Math.sqrt(T));
+        this.confHigh = this.mean + (1.96 * this.std / Math.sqrt(T));
+    }
+    public double mean(){                                       // sample mean of percolation threshold
+        return this.mean;
+    }
+    public double stddev(){                                     // sample standard deviation of percolation threshold
+        return this.std;
+    }
+    public double confidenceLow(){                              // low endpoint of 95% confidence interval
+        return this.confLow;
+    }
+    public double confidenceHigh(){                             // high endpoint of 95% confidence interval
+        return this.confHigh;
+    }
+
+    public static void main(String[] args){
+        PercolationStats s = new PercolationStats(100, 10000, new PercolationFactory());
+        System.out.println("Mean: " + s.mean() +
+                " Stddev: " + s.stddev() +
+                " ConfLow: " + s.confidenceLow() +
+                " ConfHigh: " + s.confidenceHigh());
+    }
 }
